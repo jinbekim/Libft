e95afb61c0a6929c95765d65ffa9b3005c1e55a8
JinBeom <lljrwq123@daum.net>
2020-12-28 16:26:16 +0900

    kk

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jinbekim <jinbekim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/25 22:24:16 by jinbekim          #+#    #+#             */
-/*   Updated: 2020/12/28 00:50:53 by jinbekim         ###   ########.fr       */
+/*   Updated: 2020/12/28 16:14:47 by jinbekim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,10 @@ t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*curr;
 	t_list	*new;
 
+	if (!(lst && f && del))
+		return (0);
+	curr = NULL;
+	new = NULL;
 	while (lst)
 	{
 		if (!(curr = ft_lstnew(f(lst->content))))
