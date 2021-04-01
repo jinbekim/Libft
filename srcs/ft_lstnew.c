98c5d599c83e3e58ce62d4fa2659e1cd8aa8c378
jinbekim <jinbekim@student.42seoul.kr>
2021-04-02 00:46:34 +0900

    finish

--- srcs/ft_lstnew.c
+++ srcs/ft_lstnew.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jinbekim <jinbekim@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jinbekim <jinbekim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/25 20:22:03 by jinbekim          #+#    #+#             */
-/*   Updated: 2020/12/29 17:48:43 by jinbekim         ###   ########.fr       */
+/*   Updated: 2021/04/01 21:45:33 by jinbekim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 t_list	*ft_lstnew(void *content)
 {
-	t_list *lstnew;
+	t_list	*lstnew;
 
-	if (!(lstnew = malloc(sizeof(t_list))))
+	lstnew = malloc(sizeof(t_list));
+	if (lstnew == NULL)
 		return (0);
 	lstnew->content = content;
 	lstnew->next = NULL;
